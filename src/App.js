6810cd3e8cf3292a506bb42181e7e40e57daf8e6
folivora <folivora.design@gmail.com>
2021-10-02 17:28:01 +0900

    ・default import export ・named import export

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import Article from "./components/Article";
 
 function App() {
   return (
-    <div className="App">
-      <Article
-        title={'React入門'}
-        content={'ここはコンテンツです！testok'}
-      />
-    </div>
+      <div className="App">
+        <Article
+          title={'React入門'}
+          content={'ここはコンテンツです！testok'}
+        />
+      </div>
   );
 }
 
